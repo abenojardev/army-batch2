8d1a21710db805db01a8784d20a5de1d5e77594c
abenojardev <abenojardev@gmail.com>
2021-05-18 14:29:21 +0800

    up

--- Day2/bootstrap/resources/views/layout.blade.php
+++ Day2/bootstrap/resources/views/layout.blade.php
@@ -42,7 +42,67 @@
         </div>
     </header>
     
-    <div class="container"></div>
+    <div>
+        <div style="margin-top: -1.5%;" class="row">
+            <div class="col-3">
+                <div style="height:88vh;" class="bg-dark d-flex flex-column flex-shrink-0 p-3 bg-light">
+                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+                    <span class="fs-4 text-white">Sidebar</span>
+                    </a>
+                    <hr>
+                    <ul class="nav nav-pills flex-column mb-auto">
+                    <li class="nav-item">
+                        <a href="#" class=" text-white nav-link active" aria-current="page">
+                        <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+                        Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class=" text-white nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+                        Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class=" text-white nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+                        Orders
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class=" text-white nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+                        Products
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class=" text-white nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+                        Customers
+                        </a>
+                    </li>
+                    </ul>
+                    <hr>
+                    <div class="dropdown">
+                    <a href="#" class=" text-white d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                        <strong>mdo</strong>
+                    </a>
+                    <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                        <li><a class="dropdown-item" href="#">New project...</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-9 b-light">asasdd</div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
