34a2d34ea62d94a4be8ae2510d9fae3571409ee7
abenojardev <abenojardev@gmail.com>
2021-05-19 09:52:24 +0800

    up

--- Day2/bootstrap/resources/views/components/sidebar.blade.php
+++ Day2/bootstrap/resources/views/components/sidebar.blade.php
@@ -1,37 +1,37 @@
-<div style="height:88vh;" class="bg-dark d-flex flex-column flex-shrink-0 p-3 bg-light">
+<div style="height:88vh;" class="bg-dxark d-flex flex-column flex-shrink-0 p-3 bg-light">
                     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
                     <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-                    <span class="fs-4 text-white">Sidebar</span>
+                    <span class="fs-4">Sidebar</span>
                     </a>
                     <hr>
                     <ul class="nav nav-pills flex-column mb-auto">
                     <li class="nav-item">
-                        <a href="#" class=" text-white nav-link active" aria-current="page">
+                        <a href="#" class="nav-link active" aria-current="page">
                         <!-- added for icons -->
                         <i class="fa fa-user"></i>
                         Home
                         </a>
                     </li>
                     <li>
-                        <a href="#" class=" text-white nav-link link-dark">
+                        <a href="#" class="nav-link link-dark">
                         <i class="fa fa-bar-chart"></i>
                         Dashboard
                         </a>
                     </li>
                     <li>
-                        <a href="#" class=" text-white nav-link link-dark">
+                        <a href="#" class="nav-link link-dark">
                         <i class="fa fa-list"></i>
                         Orders
                         </a>
                     </li>
                     <li>
-                        <a href="#" class=" text-white nav-link link-dark">
+                        <a href="#" class="nav-link link-dark">
                         <i class="fa fa-star"></i>
                         Products
                         </a>
                     </li>
                     <li>
-                        <a href="#" class=" text-white nav-link link-dark">
+                        <a href="#" class="nav-link link-dark">
                         <i class="fa fa-users"></i>
                         Customers
                         </a>
@@ -39,7 +39,7 @@
                     </ul>
                     <hr>
                     <div class="dropdown">
-                    <a href="#" class=" text-white d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
                         <strong>mdo</strong>
                     </a>
--- Day2/bootstrap/resources/views/layout.blade.php
+++ Day2/bootstrap/resources/views/layout.blade.php
@@ -1,88 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+<div class="row align-items-center"> 
+    <div class="col-12 mt-5">
+        <div class="card">
+            <div class="card-body row p-5"> 
 
-    <!-- added for icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-</head>
-<body class="">
-    
-    @include('components.header')
-    
-    <div class="main-content">
-        <div style="margin-top: -1.5%;" class="row">
-            <div class="col-3">
-                @include('components.sidebar')
-            </div>
-            
-            <div class="col-9 b-light">
-                <div class="row align-items-center"> 
-                    <div class="col-12 mt-5">
-                        <div class="card">
-                            <div class="card-body row p-5"> 
+                <div class="col-10">
+                    <h4>
+                        All Records 
+                    </h4>
+                </div>
+                <div class="col-2 text-end"> 
+                    <button class="btn btn-dark pull-right">Create</button>
+                </div> 
+                <div class="col-12">
+                    <hr>    
+                </div>
 
-                                <div class="col-10">
-                                    <h4>
-                                        All Records 
-                                    </h4>
-                                </div>
-                                <div class="col-2 text-end"> 
-                                    <button class="btn btn-dark pull-right">Create</button>
-                                </div> 
-                                <div class="col-12">
-                                    <hr>    
-                                </div>
+                <div class="col-8"></div>
+                <div class="col-4 text-end"> 
+                    <input type="text" class="form-control" placeholder="Search">
+                </div> 
 
-                                <div class="col-8"></div>
-                                <div class="col-4 text-end"> 
-                                    <input type="text" class="form-control" placeholder="Search">
-                                </div> 
-
-                                <div class="col-12">
-                                    <table class="table mt-3">
-                                        <thead>
-                                            <tr>
-                                                <th class="text-muted" scope="col">#</th>
-                                                <th class="text-muted" scope="col">First</th>
-                                                <th class="text-muted" scope="col">Last</th>
-                                                <th class="text-muted" scope="col">Handle</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                            </tr> 
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                            </tr> 
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                            </tr> 
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div> 
+                <div class="col-12">
+                    <table class="table mt-3">
+                        <thead>
+                            <tr>
+                                <th class="text-muted" scope="col">#</th>
+                                <th class="text-muted" scope="col">First</th>
+                                <th class="text-muted" scope="col">Last</th>
+                                <th class="text-muted" scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr> 
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr> 
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr> 
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-</body>
-</html>
+    </div> 
+</div>
