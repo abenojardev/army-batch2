ccf1456f457b7587ea5a267013df77b5c97e5539
abenojardev <abenojardev@gmail.com>
2021-05-24 15:19:25 +0800

    project payroll

--- Day5/relationship/routes/web.php
+++ Day5/relationship/routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () { 
+    $data = DB::table('song')
+                ->join('album','album.id','song.album_id')
+                ->join('genre','genre.id','song.genre_id')
+                ->join('artist','artist.id','song.artist_id')
+                ->select(
+                    'title',
+                    'album_name',
+                    'genre_name',
+                    'artist_name'
+                )
+                ->get();
+    
+    return response()->json($data, 200, [], JSON_PRETTY_PRINT);
 });
