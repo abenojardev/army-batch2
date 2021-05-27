1e785e3c3bf2dc81b14a8b41b744f87cd620e1b7
abenojardev <abenojardev@gmail.com>
2021-05-27 16:09:54 +0800

    feat: added controller

---
+++ refresher/app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Department;
+use Redirect;
+
+class DepartmentController extends Controller
+{
+    protected $request;
+    
+    public function __construct(Request $request) {
+        $this->request = $request;
+    }
+
+    public function department()
+    {
+        return view('department');
+    }
+
+    public function save()
+    {
+        Department::create(
+            $this->request->except('_token')
+        );
+
+        return Redirect::route('home');
+    }
+}
