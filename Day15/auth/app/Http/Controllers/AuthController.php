74b4226fe5b3aec6293ed8c790d26d0ad83b962b
abenojardev <abenojardev@gmail.com>
2021-06-04 16:57:02 +0800

    ad

--- Day15/auth/app/Http/Controllers/AuthController.php
+++ Day15/auth/app/Http/Controllers/AuthController.php
@@ -80,7 +80,14 @@ class AuthController extends Controller
 
     public function chat()
     {
-        return view('chat');
+        $data = Message::select('messages.*', 'users.avatar', 'users.name')
+                    ->join('users', 'users.id', 'messages.user_id')
+                    ->orderBy('messages.created_at','DESC')
+                    ->get();
+ 
+        return view('chat')->with([
+            'data' => $data
+        ]);
     }
 
     public function chat_send()
