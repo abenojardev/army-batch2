2e2830547f860d626f52b17fae3d2daee5928d5e
abenojardev <abenojardev@gmail.com>
2021-06-10 11:16:41 +0800

    up

--- Day19/mail/resources/views/index.blade.php
+++ Day19/mail/resources/views/index.blade.php
@@ -8,14 +8,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
 <body class="d-flex justify-content-center mt-5"> 
-    <form class="col-4">
+    <form class="col-4" method="post" action="{{ URL::route('send') }}">
+        @csrf
         <div class="mb-3">
             <label class="form-label">Email address</label>
-            <input type="email" class="form-control"> 
+            <input type="email" class="form-control" name="email"> 
         </div>
         <div class="mb-3">
             <label class="form-label">Message</label>
-            <textarea class="form-control" rows="5"></textarea>
+            <textarea class="form-control" rows="5" name="message"></textarea>
         </div> 
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
