f5febce57cce1f4ad7cfa5ca862273cdd9c78957
abenojardev <abenojardev@gmail.com>
2021-06-10 10:34:41 +0800

    asd

---
+++ Day19/mail/resources/views/emails/default_template.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+</head>
+<body>
+    <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+        <tr>
+            <td align="center" valign="top">
+
+            </td>
+        </tr> 
+    </table>
+</body>
+</html>
